c2569aa92253c492bd9e3f6aca7b5cdb09e81c30
Christopher Day <c.day@epcc.ed.ac.uk>
2026-02-06 13:14:28 +0000

    Initial openmp target directives into tvdadvection

--- components/tvdadvection/src/ultimateflux.F90
+++ components/tvdadvection/src/ultimateflux.F90
@@ -17,12 +17,13 @@ module ultimateflux_mod
   public ultflx
 
   real(kind=DEFAULT_PRECISION) :: r6 !< A sixth used when calculating fluxes in specific directions
+  !$omp declare target(r6)
 
 contains
 
   subroutine ultflx(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        local_grid, grid_config, parallel, kdof, dt, flux_y, flux_z, flux_x, flux_previous_x, rdz, rdzn, dzn, kmin, kmax)
-
+     !$omp declare target
     integer, intent(in) ::y_flow_index, x_flow_index, y_scalar_index, x_scalar_index   &! loop counter
          ,kdof,&                  ! =1 for advection of W, 0 otherwise to shift vertical grid index
          kmin, kmax
@@ -59,8 +60,10 @@ contains
 #endif
   end subroutine ultflx
 
+
   subroutine handle_vertical_fluxes(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        local_grid, grid_config, kdof, dt, flux_z, rdz, rdzn, dzn, kmin, kmax)
+     !$omp declare target
     integer, intent(in) :: kdof, y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, kmin, kmax
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(prognostic_field_type), intent(inout) :: u, w, v, zf
@@ -80,6 +83,7 @@ contains
 
   subroutine handle_x_direction_fluxes(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, local_grid, &
        grid_config, kdof, dt, flux_x, rdz, kmin, kmax)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, kdof, kmin, kmax
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(prognostic_field_type), intent(inout) :: u, w, v, zf
@@ -105,10 +109,11 @@ contains
       call calculate_stencil_for_u(k, advneg, advpos, fc, fcurvs, fd, fdels, fu, &
            u%data(:, y_flow_index, x_flow_index), zf%data(:, y_scalar_index, x_scalar_index-1), &
            zf%data(:, y_scalar_index, x_scalar_index+2), zf%data(:, y_scalar_index, x_scalar_index), &
-           zf%data(:, y_scalar_index, x_scalar_index+1))      
+           zf%data(:, y_scalar_index, x_scalar_index+1))
+ 
       if(abs(fcurvs) .ge. abs(fdels))then
         flux_x(k) = fc
-      else        
+      else 
         if (u%data(k, y_flow_index, x_flow_index) .gt. 0.0_DEFAULT_PRECISION) then
           sum_cfl_out=0.0_DEFAULT_PRECISION
 #ifdef W_ACTIVE
@@ -160,13 +165,14 @@ contains
 #endif
         ! Now calculate the fluxes in X
         flux_x(k) = calculate_flux_in_x(u%data(k, y_flow_index, x_flow_index), dt, sum_cfl_out, fdels, fd, fc, fu, fgt1, fgt2, &
-             grid_config%horizontal%cx, fcurvs)
+             grid_config%horizontal%cx, fcurvs) 
       end if
     end do
   end subroutine handle_x_direction_fluxes
 
   subroutine handle_y_direction_fluxes(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        local_grid, grid_config, kdof, dt, flux_y, rdz, kmin, kmax)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, kdof, kmin, kmax
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(grid_configuration_type), intent(inout) :: grid_config
@@ -235,6 +241,7 @@ contains
 
   subroutine handle_vertical_fluxes_middleofcolumn(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        local_grid, grid_config, kdof, dt, flux_z, rdz, rdzn, dzn)
+     !$omp declare target
     integer, intent(in) :: kdof, y_flow_index, x_flow_index, y_scalar_index, x_scalar_index
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(grid_configuration_type), intent(inout) :: grid_config
@@ -306,6 +313,7 @@ contains
 
   subroutine handle_vertical_fluxes_bottomofcolumn(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        grid_config, kdof, dt, flux_z, rdz, rdzn, dzn)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, kdof
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(grid_configuration_type), intent(inout) :: grid_config
@@ -412,6 +420,7 @@ contains
 
   subroutine handle_vertical_fluxes_topofcolumn(y_flow_index, x_flow_index, u, v, w, y_scalar_index, x_scalar_index, zf, &
        local_grid, grid_config, kdof, dt, flux_z, rdz, rdzn, dzn)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, kdof
     real(kind=DEFAULT_PRECISION), intent(in) :: dt
     type(grid_configuration_type), intent(inout) :: grid_config
@@ -524,6 +533,7 @@ contains
 
   real(kind=DEFAULT_PRECISION) function calculate_flux_in_w(k, data_value, dt, sum_cfl_out, rdc, fdels, fd, fc, fu, rdu, fgt1, &
        fgt2, dzn, rdzn)
+     !$omp declare target
     integer, intent(in) :: k
     real(kind=DEFAULT_PRECISION), intent(in), dimension(:) :: dzn, rdzn
     real(kind=DEFAULT_PRECISION), intent(in) :: dt, sum_cfl_out, rdc, fdels, fd, fc, fu, rdu, fgt1, fgt2
@@ -558,6 +568,7 @@ contains
   end function calculate_flux_in_w
 
   real(kind=DEFAULT_PRECISION) function calculate_flux_in_x(data_value, dt, sum_cfl_out, fdels, fd, fc, fu, fgt1, fgt2, cx, fcurvs)
+     !$omp declare target
     real(kind=DEFAULT_PRECISION), intent(in) :: sum_cfl_out, fdels, fd, fc, fu, fgt1, fgt2, cx, dt, fcurvs
     real(kind=DEFAULT_PRECISION), intent(in) :: data_value
 
@@ -586,6 +597,7 @@ contains
   end function calculate_flux_in_x
 
   real(kind=DEFAULT_PRECISION) function calculate_flux_in_y(data_value, dt, sum_cfl_out, fdels, fd, fc, fu, fgt1, fgt2, cy, fcurvs)
+     !$omp declare target
     real(kind=DEFAULT_PRECISION), intent(in) :: dt, sum_cfl_out, fdels, fd, fc, fu, fgt1, fgt2, cy, fcurvs
     real(kind=DEFAULT_PRECISION), intent(in) :: data_value
 
@@ -615,6 +627,7 @@ contains
 
   subroutine calculate_stencil_for_u(k, advneg, advpos, fc, fcurvs, fd, fdels, fu, &
        flow_field, advection_field_one, advection_field_two, advection_field_three, advection_field_four)
+     !$omp declare target
     integer, intent(in) :: k
     real(kind=DEFAULT_PRECISION), intent(in), dimension(:) :: flow_field, advection_field_one, advection_field_two, &
          advection_field_three, advection_field_four
@@ -634,6 +647,7 @@ contains
 
   subroutine calculate_stencil_for_y(y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, k, advneg, advpos, &
        fc, fcurvs, fd, fdels, fu, jofset, jperiod, flow_field, zf)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, k
     type(prognostic_field_type), intent(inout) :: flow_field, zf
     integer, intent(out) :: jofset, jperiod
@@ -657,6 +671,7 @@ contains
 
   subroutine calculate_stencil_for_w(y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, k, advneg, advpos, &
        fc, fcurvs, fd, fdels, fu, kofset, flow_field, zf)
+     !$omp declare target
     integer, intent(in) :: y_flow_index, x_flow_index, y_scalar_index, x_scalar_index, k
     type(prognostic_field_type), intent(inout) :: flow_field, zf
     integer, intent(out) :: kofset
@@ -677,6 +692,7 @@ contains
 
   real(kind=DEFAULT_PRECISION) function calculate_gradient_term_in_z(k, column_one, column_two, dt, advneg, &
        advpos, kdof, kp0, kp1, source_column_one, source_column_two, rdz)
+     !$omp declare target
     integer, intent(in) :: k, kdof, kp0, kp1
     real(kind=DEFAULT_PRECISION), intent(in) :: dt, advneg, advpos
     real(kind=DEFAULT_PRECISION), intent(in), dimension(:) :: column_one, column_two, source_column_one
@@ -697,6 +713,7 @@ contains
 
   real(kind=DEFAULT_PRECISION) function calculate_gradient_term_in_x(k, column_one, column_two, column_three, column_four, &
        cx, dt, fc, source_value_one, source_value_two, kpv)
+     !$omp declare target
     integer, intent(in) :: k, kpv
     real(kind=DEFAULT_PRECISION), intent(in) :: cx, dt, fc
     real(kind=DEFAULT_PRECISION), intent(in) :: source_value_one, source_value_two
@@ -713,7 +730,7 @@ contains
        dt, fc, term_data_one, term_data_one_y_index, term_data_one_x_index, koffset_one, multiplication_factor_one, &
        term_data_two, term_data_two_y_index, term_data_two_x_index, &
        koffset_two, multiplication_factor_two, data_two, data_two_y_index, data_two_x_index)
-
+     !$omp declare target
     integer, intent(in) :: k, data_y_index, data_x_index, term_data_one_y_index, term_data_one_x_index
     integer, intent(in), optional :: term_data_two_y_index, term_data_two_x_index, data_two_y_index, data_two_x_index
     integer, intent(in), optional :: koffset_one, koffset_two
